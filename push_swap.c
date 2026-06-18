dd03f12c0262ab0bca9436271f9c257b2dc51927
zleullie <zleullie@student.42sp.org.br>
2026-06-18 00:35:32 -0300

    fix: properly error out

--- push_swap.c
+++ push_swap.c
@@ -15,11 +15,11 @@
 #include "util.h"
 #include <stdlib.h>
 
-typedef void	(*t_sorting_func)(t_stack *a, t_stack *b);
+typedef void (*t_sorting_func)(t_stack *a, t_stack *b);
 
-static t_sorting_func	pick_algorithm(struct s_args *args)
+static t_sorting_func pick_algorithm(struct s_args *args)
 {
-	double	disorder;
+	double disorder;
 
 	disorder = compute_disorder(args->numbers, args->size);
 	if (args->flags & ADAPTIVE)
@@ -43,29 +43,29 @@ static t_sorting_func	pick_algorithm(struct s_args *args)
 	return (chunk_sort);
 }
 
-static int	error_out(void)
+static int error_out(void)
 {
 	ft_printf("Error\n");
 	return (1);
 }
 
-int	main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	struct s_args	args;
-	t_stack			stack_a;
-	t_stack			stack_b;
-	t_sorting_func	func;
+	struct s_args args;
+	t_stack stack_a;
+	t_stack stack_b;
+	t_sorting_func func;
 
 	if (argc == 1)
 		return (0);
 	stack_init(&stack_a);
 	stack_init(&stack_b);
 	if (!initialize_args(&args, argc, argv))
-		error_out();
+		return (error_out());
 	stack_from_list(&args, &stack_a);
 	func = pick_algorithm(&args);
 	if (!func)
-		error_out();
+		return (error_out());
 	func(&stack_a, &stack_b);
 	if (args.flags & BENCH)
 		bench(&args);
